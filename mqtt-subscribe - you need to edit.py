831316a6264a01ca6dc62bd00a830513892b8a28
KayserSosa <npkayser@gmail.com>
2022-05-21 16:16:39 -0500

    Add files via upload

--- mqtt-subscribe - you need to edit.py
+++ mqtt-subscribe - you need to edit.py
@@ -14,7 +14,7 @@
 # Imports ======================================================================================================================
 import random
 import os
-import logging
+import time
 from paho.mqtt import client as mqtt_client
 
 
@@ -32,26 +32,22 @@ subscribetopic = [('etekcityzap/switch1/command',1),('etekcityzap/switch2/comman
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 
 
-# MQTT Log File ================================================================================================================
-FORMAT = '%(asctime)s - %(user) - %(message)s'
-logging.basicConfig(filename='/home/pi/mqtt-rf/mqtt.log', level=logging.DEBUG, format=FORMAT)
-#^^^^^^^^^^^^^^^^^^^^^ log file that will contain all the same stuff as the print() commands with a time/date stamp, you will ahve to manually empty the file when too big
-
-
 # MQTT Connection ==============================================================================================================
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
-            logging.debug("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-            logging.debug("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
-    client.connect(broker, port)
+
+    #client.connect(broker, port)
+    client.connect(broker, port, keepalive=60)
+    #http://www.steves-internet-guide.com/mqtt-keep-alive-by-example/
+
     return client
 
 
@@ -59,7 +55,6 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        logging.debug(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         
         topic = msg.topic
         message = msg.payload.decode()
